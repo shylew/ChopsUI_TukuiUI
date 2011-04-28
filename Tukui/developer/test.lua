21c94614f07b7df1c3e69dca095edacd0fe0f02f
Tukz <tukz@tukui.org>
2011-04-28 15:42:52 -0400

    This is a quick attempt to reskin all battleground timers bar to match Tukui. Currently for testing purpose only.

--- Tukui/developer/test.lua
+++ Tukui/developer/test.lua
@@ -1,1 +1,59 @@
-local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
+local T, C, L = unpack(select(2, ...)) -- Import Functions/Constants, Config, Locales
+
+--------------------------------------------------------------------------
+-- This code is for testing purpose
+-- it reskin the all "Battleground" Timer bars.
+--------------------------------------------------------------------------
+
+local function SkinIt(self)	
+	local name = self:GetName()
+	local bar = _G[name.."StatusBar"]
+	print(name)
+
+	bar:ClearAllPoints()
+	bar:Point("TOPLEFT", self, "TOPLEFT", 2, -2)
+	bar:Point("BOTTOMRIGHT", self, "BOTTOMRIGHT", -2, 2)
+		
+	for i=1, bar:GetNumRegions() do
+		local region = select(i, bar:GetRegions())
+		if region:GetObjectType() == "Texture" then
+			if not region:GetTexture() ~= C["media"].normTex then
+				region:SetTexture(nil)
+			end
+		elseif region:GetObjectType() == "FontString" then
+			region:SetFont(C["media"].font, 12, "THINOUTLINE")
+		end
+	end
+	
+	bar:SetStatusBarTexture(C["media"].normTex)
+	bar:SetStatusBarColor(170/255, 10/255, 10/255)
+	
+	bar.backdrop = CreateFrame("Frame", nil, bar)
+	bar.backdrop:SetFrameLevel(0)
+	bar.backdrop:SetTemplate("Default")
+	bar.backdrop:SetAllPoints(self)
+end
+
+local function SkinBlizzTimer(self, event, ...)
+	if event == "START_TIMER" then
+		local bar
+		local isTimerRuning = false
+		local timerType, timeSeconds, totalTime = ...
+
+		for a,b in pairs(self.timerList) do
+			if b.type == timerType and not b.isFree then
+				bar = b
+				isTimerRuning = true
+				break
+			end
+		end
+		
+		if isTimerRuning then 
+			for a,b in pairs(bar) do
+				SkinIt(bar)
+			end
+		end
+	end
+end
+
+hooksecurefunc("TimerTracker_OnEvent", SkinBlizzTimer)
