d4d64a284d783928d7cfc267b7daab7859de97a1
Tukz <tukz@tukui.org>
2011-05-05 14:24:14 -0400

    Oups, Blizzard_DebugTools skin was removed accidently.

--- Tukui/modules/skins/blizzardframes.lua
+++ Tukui/modules/skins/blizzardframes.lua
@@ -560,9 +560,60 @@ TukuiSkin:SetScript("OnEvent", function(self, event, addon)
 		for i=1,GetNumTalents(1,false,true) do
 			PetTalentButtons(nil,true,i)
 		end
-
 	end
 
+	if addon == "Blizzard_DebugTools" then
+		local noscalemult = T.mult * C["general"].uiscale
+		local bg = {
+		  bgFile = C["media"].blank, 
+		  edgeFile = C["media"].blank, 
+		  tile = false, tileSize = 0, edgeSize = noscalemult, 
+		  insets = { left = -noscalemult, right = -noscalemult, top = -noscalemult, bottom = -noscalemult}
+		}
+		
+		ScriptErrorsFrame:SetBackdrop(bg)
+		ScriptErrorsFrame:SetBackdropColor(unpack(C.media.backdropcolor))
+		ScriptErrorsFrame:SetBackdropBorderColor(unpack(C.media.bordercolor))	
+
+		EventTraceFrame:SetTemplate("Transparent")
+		
+		local texs = {
+			"TopLeft",
+			"TopRight",
+			"Top",
+			"BottomLeft",
+			"BottomRight",
+			"Bottom",
+			"Left",
+			"Right",
+			"TitleBG",
+			"DialogBG",
+		}
+		
+		for i=1, #texs do
+			_G["ScriptErrorsFrame"..texs[i]]:SetTexture(nil)
+			_G["EventTraceFrame"..texs[i]]:SetTexture(nil)
+		end
+		
+		local bg = {
+		  bgFile = C["media"].blank, 
+		  edgeFile = C["media"].blank, 
+		  tile = false, tileSize = 0, edgeSize = noscalemult, 
+		  insets = { left = -noscalemult, right = -noscalemult, top = -noscalemult, bottom = -noscalemult}
+		}
+		
+		for i=1, ScriptErrorsFrame:GetNumChildren() do
+			local child = select(i, ScriptErrorsFrame:GetChildren())
+			if child:GetObjectType() == "Button" and not child:GetName() then
+				
+				SkinButton(child)
+				child:SetBackdrop(bg)
+				child:SetBackdropColor(unpack(C.media.backdropcolor))
+				child:SetBackdropBorderColor(unpack(C.media.bordercolor))	
+			end
+		end	
+	end
+	
 	if addon == "Blizzard_GlyphUI" then
 
 		--GLYPHS TAB
