9dc7c248b5dd9156b3f708f03b6448158513e299
Tukz <tukz@tukui.org>
2011-05-15 04:26:50 -0400

    This was tainting spellbook spell click.

--- Tukui/modules/skins/blizzardframes.lua
+++ Tukui/modules/skins/blizzardframes.lua
@@ -76,12 +76,6 @@ local function SkinTab(tab)
 	tab.backdrop:SetFrameLevel(tab:GetFrameLevel() - 1)
 	tab.backdrop:Point("TOPLEFT", 10, -3)
 	tab.backdrop:Point("BOTTOMRIGHT", -10, 3)
-	
-	-- always set tab text centered
-	local name = tab:GetName()
-	_G[name.."Text"]:ClearAllPoints()
-	_G[name.."Text"]:SetPoint("CENTER")
-	_G[name.."Text"].SetPoint = T.dummy
 end
 
 local function SkinNextPrevButton(btn, horizonal)
