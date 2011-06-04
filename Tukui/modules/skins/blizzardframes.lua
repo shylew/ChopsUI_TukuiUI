ebc9c23bff57d1b56e560df95082d4f7773d6734
Tukz <tukz@tukui.org>
2011-06-04 15:27:00 -0400

    Bugs Fixes : Fix all taints errors on WorldMap.

--- Tukui/modules/skins/blizzardframes.lua
+++ Tukui/modules/skins/blizzardframes.lua
@@ -3602,12 +3602,57 @@ TukuiSkin:SetScript("OnEvent", function(self, event, addon)
 			hooksecurefunc("WorldMap_ToggleSizeUp", FixSkin)
 			
 			WorldMapFrame:RegisterEvent("PLAYER_LOGIN")
+			WorldMapFrame:RegisterEvent("PLAYER_REGEN_ENABLED") -- fix taint with small map & big map
+			WorldMapFrame:RegisterEvent("PLAYER_REGEN_DISABLED") -- fix taint with small map & big map
 			WorldMapFrame:HookScript("OnEvent", function(self, event)
 				if event == "PLAYER_LOGIN" then
 					if not GetCVarBool("miniWorldMap") then
 						ToggleFrame(WorldMapFrame)
 						ToggleFrame(WorldMapFrame)
 					end
+				elseif event == "PLAYER_REGEN_DISABLED" then
+					if GetCVarBool("miniWorldMap") then
+						WorldMapFrameSizeDownButton:Disable()
+						WorldMapFrameSizeUpButton:Disable()
+						
+						HideUIPanel(WorldMapFrame)
+						WatchFrame.showObjectives = nil
+						WorldMapQuestShowObjectives:SetChecked(false)
+						WorldMapTitleButton:Hide()
+						WorldMapBlobFrame:Hide()
+						WorldMapPOIFrame:Hide()
+
+						WorldMapQuestShowObjectives.Show = T.dummy
+						WorldMapTitleButton.Show = T.dummy
+						WorldMapBlobFrame.Show = T.dummy
+						WorldMapPOIFrame.Show = T.dummy       
+
+						WatchFrame_Update()
+					end
+					
+					WorldMapQuestShowObjectives:Hide() -- don't allow disabling quest track in combat on both map, cause lots of taints
+				elseif event == "PLAYER_REGEN_ENABLED" then
+					if GetCVarBool("miniWorldMap") then
+						WorldMapFrameSizeDownButton:Enable()
+						WorldMapFrameSizeUpButton:Enable()
+						
+						WorldMapQuestShowObjectives.Show = WorldMapQuestShowObjectives:Show()
+						WorldMapTitleButton.Show = WorldMapTitleButton:Show()
+						WorldMapBlobFrame.Show = WorldMapBlobFrame:Show()
+						WorldMapPOIFrame.Show = WorldMapPOIFrame:Show()
+
+						WorldMapTitleButton:Show()
+
+						WatchFrame.showObjectives = true
+						WorldMapQuestShowObjectives:SetChecked(true)
+
+						WorldMapBlobFrame:Show()
+						WorldMapPOIFrame:Show()
+
+						WatchFrame_Update()
+					end
+					
+					WorldMapQuestShowObjectives:Show()
 				end
 			end)
 			
@@ -3625,15 +3670,6 @@ TukuiSkin:SetScript("OnEvent", function(self, event, addon)
 			local int = 0
 			
 			WorldMapFrame:HookScript("OnUpdate", function(self, elapsed)
-				--For some reason these buttons aren't functioning correctly, and we can't afford for it to fuckup because toggling to a big map in combat will cause a taint.
-				if InCombatLockdown() then
-					WorldMapFrameSizeDownButton:Disable()
-					WorldMapFrameSizeUpButton:Disable()
-				else
-					WorldMapFrameSizeDownButton:Enable()
-					WorldMapFrameSizeUpButton:Enable()			
-				end
-				
 				if WORLDMAP_SETTINGS.size == WORLDMAP_FULLMAP_SIZE then
 					WorldMapFrameSizeUpButton:Hide()
 					WorldMapFrameSizeDownButton:Show()
