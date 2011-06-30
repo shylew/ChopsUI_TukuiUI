664404db6a259d8b2d49c942bfb2109e18b553b4
Trond Arve Nordheim <tanordheim@gmail.com>
2011-06-30 10:34:27 +0200

    Added a Ruby support script to automatically patch BigWigs to work with ChopsUI

---
+++ support/patch_bigwigs.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+def patch_loader
+  
+  lua = File.open(File.join('BigWigs', 'Loader.lua'), 'r').read
+
+  unless lua =~ /function BigWigs_LoadAndEnableCore/
+
+    # Add ChopsUI external loader code
+    lua << "\n-- ChopsUI hack to allow external initialization of BigWigs\r\n"
+    lua << "function BigWigs_LoadAndEnableCore()\r\n"
+    lua << "  loadAndEnableCore()\r\n"
+    lua << "end\r\n"
+
+    File.open(File.join('BigWigs', 'Loader.lua'), 'w') { |f| f.puts lua }
+
+  end
+
+end
+
+def patch_bars_plugin
+
+  lua = File.open(File.join('BigWigs_Plugins', 'Bars.lua'), 'r').read
+
+  unless lua =~ /ChopsuiBigWigsReposition/
+
+    # Add a call to ChopsUI to reposition the frames during initizliation
+    lua.gsub!(/display\:RefixPosition\(\)/, "display:RefixPosition\r\n  ChopsuiBigWigsReposition(frameName)")
+
+    File.open(File.join('BigWigs_Plugins', 'Bars.lua'), 'w') { |f| f.puts lua }
+
+  end
+
+end
+
+def patch_messages_plugin
+
+  lua = File.open(File.join('BigWigs_Plugins', 'Messages.lua'), 'r').read
+
+  unless lua =~ /ChopsuiBigWigsReposition/
+
+    # Add a call to ChopsUI to reposition the frames during initizliation
+    lua.gsub!(/display\:RefixPosition\(\)/, "display:RefixPosition\r\n  ChopsuiBigWigsReposition(frameName)")
+
+    File.open(File.join('BigWigs_Plugins', 'Messages.lua'), 'w') { |f| f.puts lua }
+
+  end
+
+end
+
+patch_loader
+patch_bars_plugin
+patch_messages_plugin
