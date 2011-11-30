4f8d0b1cda183ac54b80c886809afc4be140ce90
Trond Arve Nordheim <tanordheim@gmail.com>
2011-11-30 11:48:21 +0100

    Updating GridStatusRaidDebuff to the latest version

--- GridStatusRD_Cata/BaradinHold.lua
+++ GridStatusRD_Cata/BaradinHold.lua
@@ -5,11 +5,22 @@ local zone = "Baradin Hold"
 --Trash
 -- Demon Containment Unit
 GridStatusRaidDebuff:Debuff(zone, 89354, 1, 6, 5) --Arcane Amplifier
+-- Disciple of Hate
+GridStatusRaidDebuff:Debuff(zone, 105859, 1, 5, 5) --Run Through
 
 --Argaloth
 GridStatusRaidDebuff:BossName(zone, 10, "Argaloth")
 GridStatusRaidDebuff:Debuff(zone, 88942, 11, 5, 5, true, true) --Meteor Slash
-GridStatusRaidDebuff:Debuff(zone, 88954, 12, 5, 5) --Consuming Darkness
---GridStatusRaidDebuff:Debuff(zone, 95172, 12, 5, 5) --Meteor Slash
---GridStatusRaidDebuff:Debuff(zone, 95173, 14, 5, 5) --Consuming Darkness
+GridStatusRaidDebuff:Debuff(zone, 88954, 12, 6, 6) --Consuming Darkness
+
+--Occu'thar
+GridStatusRaidDebuff:BossName(zone, 20, "Occu'thar")
+GridStatusRaidDebuff:Debuff(zone, 96913, 21, 5, 5, true, true) --Searing Shadows
+-- Eye of Occu'thar
+GridStatusRaidDebuff:Debuff(zone, 97028, 22, 6, 6) --Gaze of Occu'thar
+
+--Alizabal
+-- GridStatusRaidDebuff:BossName(zone, 30, "Alizabal")
+-- GridStatusRaidDebuff:Debuff(zone, 104936, 31, 4, 4) --Skewer
+-- GridStatusRaidDebuff:Debuff(zone, 105067, 32, 6, 6) --Seething Hate
 
