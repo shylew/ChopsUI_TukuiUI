f3376b5639f637df71451e8be86104d25375a8e1
Trond Arve Nordheim <tanordheim@gmail.com>
2011-11-30 11:50:44 +0100

    Removing TukUI from the bundle, it should now be installed separately.

--- Tukui/medias/sharedmedia.lua
+++
@@ -1,5 +0,0 @@
-local LSM = LibStub("LibSharedMedia-3.0")
-if LSM == nil then return end
-
-LSM:Register("statusbar", "TukuiNormalTexture", [[Interface\AddOns\Tukui\medias\textures\normTex]])
-LSM:Register("font", "TukuiNormalFont", [[Interface\Addons\Tukui\medias\fonts\normal_font.ttf]])
