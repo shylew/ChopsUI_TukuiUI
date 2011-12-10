65c51431f4c79e82b249a7825e1119032f4dcd16
Trond Arve Nordheim <tanordheim@gmail.com>
2011-12-10 11:29:15 +0100

    Adding patch for removing the bot check in TSM_Auctioning - not because I'm botting, but because I'm easily annoyed.

---
+++ __support/patch_tradeskillmaster_auctioning.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+def patch_bot_check
+
+  # I don't bot, but this check is fucking annoying when you post a million
+  # times a day and you get this check like 15 times in a row.
+  lua = File.open(File.join('TradeSkillMaster_Auctioning', 'modules', 'post.lua'), 'r').read
+  if lua =~ %r|if info ~= rNum and ShouldCheck\(\)|
+
+    lua.gsub!(%r|if info ~= rNum and ShouldCheck\(\)|, 'if info ~= rNum and true == false')
+
+    File.open(File.join('TradeSkillMaster_Auctioning', 'modules', 'post.lua'), 'w') { |f| f.puts lua }
+
+  end
+  
+end
+
+patch_bot_check
