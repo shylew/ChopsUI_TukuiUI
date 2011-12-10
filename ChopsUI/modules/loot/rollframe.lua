1ac166628d9f298c254fc0a729f07721ccf45bc6
Trond Arve Nordheim <tanordheim@gmail.com>
2011-12-10 11:20:34 +0100

    Moving the loot frame slightly up on the screen to prevent it falling behind grid in 25man raids

--- ChopsUI/modules/loot/rollframe.lua
+++ ChopsUI/modules/loot/rollframe.lua
@@ -2,3 +2,6 @@ local T, C, L = unpack(Tukui)
 
 -- We actually don't need to move this at the moment. Keeping this file here for
 -- easy access if we do later.
+local frame = TukuiRollAnchor
+frame:ClearAllPoints()
+frame:SetPoint('BOTTOM', InvTukuiActionBarBackground, 'TOP', 0, 350)
