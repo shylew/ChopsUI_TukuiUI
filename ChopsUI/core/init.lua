0a03c6e4d2f4ee3265e27671af516b4e1b7a7107
Trond Arve Nordheim <t@binarymarbles.com>
2012-01-24 09:28:44 +0100

    Adding a small module to disable movie/cinematic playback since its crashing the game in Dragon Soul

--- ChopsUI/core/init.lua
+++ ChopsUI/core/init.lua
@@ -7,7 +7,9 @@ ChopsUI = {
 -- Register a ChopsUI module.
 ChopsUI.RegisterModule = function(name, dependency)
 
-  assert(dependency, "ChopsUI " .. name .. " module failed to load, make sure all dependencies are enabled.")
+  if dependency ~= nil then
+    assert(dependency, "ChopsUI " .. name .. " module failed to load, make sure all dependencies are enabled.")
+  end
 
   ChopsUI["modules"][name] = {
     ["Reset"] = function() end
