df2226d1d45871f38280bd8cdbecee884e162a76
Trond Arve Nordheim <tanordheim@gmail.com>
2012-09-09 20:57:35 +0200

    Removing debug message

--- ChopsUI/modules/unitframes/maintank.lua
+++ ChopsUI/modules/unitframes/maintank.lua
@@ -1,12 +1,6 @@
 local T, C, L = unpack(Tukui)
 if C.unitframes.enable ~= true then return end
 
-if C.unitframes.maintank == true then
-  DEFAULT_CHAT_FRAME:AddMessage("Tukui maintank enabled")
-else
-  DEFAULT_CHAT_FRAME:AddMessage("Tukui maintank disabled")
-end
-
 local frame = TukuiMainTank
 
 -- Reposition the main tank frame.
