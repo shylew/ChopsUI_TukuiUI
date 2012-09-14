0405345449d2acc17ca2449f1b895c7ca6acf494
Trond Arve Nordheim <tanordheim@gmail.com>
2012-09-14 08:12:03 +0200

    Filemodes adjusted due to dropbox syncing, and fixing a minimap scaling issue

--- ChopsUI/modules/maps/minimap.lua
+++ ChopsUI/modules/maps/minimap.lua
@@ -7,3 +7,10 @@ frame:Point("BOTTOMRIGHT", TukuiInfoRight, "TOPRIGHT", 0, 6)
 
 -- Increase the map size.
 frame:Size(165)
+Minimap:SetParent("UIParent")
+Minimap:ClearAllPoints()
+Minimap:Size(frame:GetWidth() - 4, frame:GetHeight() - 4)
+Minimap:SetParent(TukuiMinimap)
+Minimap:ClearAllPoints()
+Minimap:Point("TOPLEFT", 2, -2)
+Minimap:Point("BOTTOMRIGHT", -2, 2)
