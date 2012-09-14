defea450616901bb590b11848e417ea43f2c2fa5
Trond Arve Nordheim <tanordheim@gmail.com>
2012-09-14 08:28:03 +0200

    Adding error filtering module

---
+++ ChopsUI/modules/chopsui/error.lua
@@ -0,0 +1,20 @@
+local T, C, L, G = unpack(Tukui)
+
+local filtered_errors = {
+  [ERR_ITEM_COOLDOWN] = true,
+  [ERR_SPELL_COOLDOWN] = true,
+  [ERR_OUT_OF_ENERGY] = true,
+  [ERR_OUT_OF_FOCUS] = true,
+  [ERR_OUT_OF_MANA] = true,
+  [ERR_OUT_OF_RAGE] = true,
+  [ERR_OUT_OF_RANGE] = true
+}
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("UI_ERROR_MESSAGE")
+UIErrorsFrame:UnregisterEvent("UI_ERROR_MESSAGE")
+frame:SetScript("OnEvent", function(self, event, error)
+  if filtered_errors[error] == nil then
+    UIErrorsFrame:AddMessage(error, 1, 0 ,0)
+  end
+end)
