c41a86f4ed5cf0fc769e33a44e1efe6134e6102f
Trond Arve Nordheim <tanordheim@gmail.com>
2012-09-16 13:46:45 +0200

    Reanchoring primary action bar to make more logical sense when having a vehicle bar and similar

--- ChopsUI/modules/actionbars/Bar1.lua
+++ ChopsUI/modules/actionbars/Bar1.lua
@@ -8,12 +8,22 @@ frame:SetScript("OnEvent", function(self)
   for i = 1, 12 do
     local button = _G["ActionButton" .. i]
     local button2 = _G["ActionButton" .. i-1]
+    button:ClearAllPoints()
     if i == 1 then
-      button:SetPoint("BOTTOMLEFT", TukuiBar1, T.buttonspacing, T.buttonspacing)
-    elseif i == 7 then
       button:SetPoint("TOPLEFT", TukuiBar1, T.buttonspacing, -T.buttonspacing)
+    elseif i == 7 then
+      --button:Hide()
+      button:SetPoint("BOTTOMLEFT", TukuiBar1, T.buttonspacing, T.buttonspacing)
     else
+      --button:Hide()
       button:SetPoint("LEFT", button2, "RIGHT", T.buttonspacing, 0)
     end
+    --if i == 1 then
+    --  button:SetPoint("TOPLEFT", TukuiBar1, T.buttonspacing, T.buttonspacing)
+    --elseif i == 7 then
+    --  button:SetPoint("TOPLEFT", TukuiBar1, T.buttonspacing, -T.buttonspacing)
+    --else
+    --  button:SetPoint("LEFT", button2, "RIGHT", T.buttonspacing, 0)
+    --end
   end
 end)
